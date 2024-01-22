e8b2d5305a9366831d9c577fef430bf8c58e8feb
minchih7 <vll7valarmorghulis@gmail.com>
2024-01-22 16:52:18 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly MyDBContext _dbContext;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ApiController(MyDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Cities()
+        {
+            var cities = _dbContext.Addresses.Select(x => x.City).Distinct();
+            return Json(cities);
+        }
+    }
+}
