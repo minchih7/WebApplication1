832d18263964b86e4a72bcd67de70725b6644bae
minchih7 <vll7valarmorghulis@gmail.com>
2024-01-28 12:11:25 +0800

    f

---
+++ WebApplication1/Controllers/GETPController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
+
+namespace WebApplication1.Controllers
+{
+    public class GETPController : Controller
+    {
+        //public IActionResult GETP()
+        //{
+        //    var databasePassword = Environment.GetEnvironmentVariable("DATABASE_PASSWORD");
+
+        //    if (databasePassword == null)
+        //    {
+        //        return Json("Environment variable not set");
+        //    }
+
+        //    return Json(databasePassword);
+        //}
+    }
+}
